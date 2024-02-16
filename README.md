889178d016ff8f43689bb6cb0782796b7281429c
AyushBagodar <ayushkumarcool3@gmail.com>
2024-02-16 17:32:23 +0530

    list-product and images

--- README.md
+++ README.md
@@ -10,3 +10,8 @@
    2. Create github account
    3. Connect vs code with github
    4. publish repository
+5. List Products
+   1. create products array
+   2. add product images
+   3. render products
+   4. style products
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
+import './App.css'
+import { sampleProducts } from './data'
 function App() {
   return (
     <>
       <header>TS Amazona</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reseved</footer>
     </>
   )
